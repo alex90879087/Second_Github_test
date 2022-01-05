58bee6d36a4f767c6442190942d1d4d44a9757b8
rararu <alex90879087@gmail.com>
2022-01-05 20:34:33 +0800

    second commit and first push

--- src/A.java
+++ src/A.java
@@ -1,6 +1,6 @@
 public class A {
     public static void main(String[] args) {
-        System.out.println("I love refrigerator");
-        System.out.println(123);
+        System.out.println("second commit and first push");
     }
+
 }
